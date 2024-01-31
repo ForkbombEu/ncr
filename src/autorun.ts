2f7c94ee306751fdcb165695ce7ba520beeb1b60
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-31 10:44:05 +0100

    feat: autorun information is now in metadata

--- src/autorun.ts
+++ src/autorun.ts
@@ -1,8 +1,9 @@
-import { config } from './cli.js';
 import { join, parse } from 'node:path';
 import fs from 'fs-extra';
 import { Type as T, Static } from '@sinclair/typebox';
 import { Value } from '@sinclair/typebox/value';
+import _ from 'lodash';
+import { readFileContent, readJsonObject, updateJsonObjectFile } from './filesystemUtils.js';
 
 //@ts-ignore
 import { Slangroom } from '@slangroom/core';
@@ -11,40 +12,33 @@ import { wallet } from '@slangroom/wallet';
 //@ts-ignore
 import { http } from '@slangroom/http';
 
+import { config } from './cli.js';
+const L = config.logger;
+
 /* Main */
+
+const AUTORUN_CONTRACTS_PATH = join(config.zencodeDirectory, '.autorun');
 
 export async function autorunContracts() {
 	await autorunInstallContracts();
 	await autorunStartupContracts();
 }
-
-/* Constants */
-
-const AUTORUN_STARTUP_PATH = join(config.zencodeDirectory, '.autorun');
-const AUTORUN_INSTALL_PATH = join(AUTORUN_STARTUP_PATH, 'once');
-
-const NO_RUN_PREFIX = '_@done';
-
-const L = config.logger;
-
-/* */
-
 export async function autorunStartupContracts() {
-	const contracts = await loadContractsData(AUTORUN_STARTUP_PATH);
-	const contractsPromises = contracts.map((c) => runContractData(c));
-	await Promise.all(contractsPromises);
+	const contracts = await loadContractsData(AUTORUN_CONTRACTS_PATH);
+	const startupContracts = contracts.filter(isStartupContract);
+	const contractsPromises = startupContracts.map(runContractData);
+	await Promise.allSettled(contractsPromises);
 }
 
 export async function autorunInstallContracts() {
-	const contracts = await loadContractsData(AUTORUN_INSTALL_PATH);
-	const contractsToRun = contracts.filter((c) => !c.path.includes(NO_RUN_PREFIX));
-	const contractsPromises = contractsToRun.map((c) => runContractData(c));
-	await Promise.all(contractsPromises);
-	const decommissionContractPromises = contractsToRun.map((c) => decommissionContract(c.path));
-	await Promise.all(decommissionContractPromises);
+	const contracts = await loadContractsData(AUTORUN_CONTRACTS_PATH);
+	const installContracts = contracts.filter(isInstallContract);
+	const installContractsToRun = installContracts.filter(isInstallContractNotExecuted);
+	const contractsPromises = installContractsToRun.map(runInstallContractData);
+	await Promise.allSettled(contractsPromises);
 }
 
-//
+/* Definitions */
 
 async function getContractPathsInDirectory(directoryPath: string): Promise<string[]> {
 	const directory = await fs.readdir(directoryPath);
@@ -73,9 +67,13 @@ async function runContractData({ contract, keys, conf, data, path }: ContractDat
 	}
 }
 
-async function decommissionContract(contractPath: string) {
-	const { dir, name, ext } = parse(contractPath);
-	renameFile(contractPath, join(dir, `${name}${NO_RUN_PREFIX}${ext}`));
+async function runInstallContractData(contractData: InstallContractData) {
+	try {
+		await runContractData(contractData);
+		await decommissionInstallContract(contractData);
+	} catch (e) {
+		L.error(e);
+	}
 }
 
 //
@@ -94,16 +92,16 @@ async function loadContractData(contractPath: string): Promise<ContractData | un
 		const contract = await readContract(contractPath);
 		if (!contract) throw new Error('Contract not found');
 
-		const { base: contractName, dir: directoryPath } = parse(contractPath);
+		const { name: contractName, dir: directoryPath } = parse(contractPath);
 
 		const keysPath = join(directoryPath, `${contractName}.keys.json`);
-		const keys = await readJson(keysPath);
+		const keys = await readJsonObject(keysPath);
 
 		const dataPath = join(directoryPath, `${contractName}.data.json`);
-		const data = await readJson(dataPath);
+		const data = await readJsonObject(dataPath);
 
 		const metadataPath = join(directoryPath, `${contractName}.metadata.json`);
-		const metadata = await readJson(metadataPath);
+		const metadata = await readJsonObject(metadataPath);
 
 		const confPath = join(directoryPath, `${contractName}.conf`);
 		const conf = await readFileContent(confPath);
@@ -133,26 +131,44 @@ function formatContract(baseContract: string): string {
 	return `Rule unknown ignore\nRule check version ${config.zenroomVersion}\n${baseContract}`;
 }
 
-async function readJson(path: string): Promise<any> {
-	const content = await readFileContent(path);
-	return content ? JSON.parse(content) : undefined;
-}
-
-async function readFileContent(
-	path: string,
-	encoding: BufferEncoding = 'utf-8'
-): Promise<string | undefined> {
-	try {
-		return (await fs.readFile(path)).toString(encoding);
-	} catch (e) {
-		return undefined;
-	}
-}
-
-async function renameFile(oldPath: string, newPath: string) {
-	await fs.move(oldPath, newPath);
-}
-
 function isZencodeFile(filePath: string): boolean {
 	return parse(filePath).ext === '.zen';
+}
+
+/* Autorun checks */
+
+const autorunStartupContractMetadataSchema = T.Object({
+	autorun: T.Literal('startup')
+});
+
+function isStartupContract(contractData: ContractData): boolean {
+	if (!contractData.metadata) return false;
+	return Value.Check(autorunStartupContractMetadataSchema, contractData.metadata);
+}
+
+//
+
+const installContractMetadataSchema = T.Object({
+	autorun: T.Literal('install'),
+	executed: T.Optional(T.Boolean())
+});
+
+interface InstallContractData extends ContractData {
+	metadata: Static<typeof installContractMetadataSchema>;
+}
+
+function isInstallContract(contractData: ContractData): contractData is InstallContractData {
+	const { metadata } = contractData;
+	return Value.Check(installContractMetadataSchema, metadata);
+}
+
+function isInstallContractNotExecuted(contractData: InstallContractData): boolean {
+	return !Boolean(contractData.metadata.executed);
+}
+
+async function decommissionInstallContract(contractData: InstallContractData) {
+	const { dir, name } = parse(contractData.path);
+	const oldMetadata = contractData.metadata;
+	const metadataPath = join(dir, `${name}.metadata.json`);
+	await updateJsonObjectFile(metadataPath, { ...oldMetadata, executed: true });
 }
