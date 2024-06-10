2be4fb481529fb3e47e62efd43cd80f3fd5ed9ef
matteo-cristino <cristino.matteo97@gmail.com>
2024-06-10 13:53:24 +0200

    feat: pass data in input to slangroom-chain

---
+++ tests/fixtures/chain/hello_with_input.chain.js
@@ -0,0 +1,10 @@
+() => {
+    return {
+        "steps": [
+            {
+                "id": "first",
+                "zencode": "Given I have a 'string' named 'say_hi'\nThen print 'say_hi'"
+            }
+        ]
+    }
+}
