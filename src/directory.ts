8ddf67987d808f8c2c00ced1aaf6ec1db4e0b459
matteo-cristino <cristino.matteo97@gmail.com>
2024-07-31 18:48:25 +0200

    refactor(directory): add one event handler for each event emitted from live-directory

--- src/directory.ts
+++ src/directory.ts
@@ -103,9 +103,19 @@ export class Directory {
 		this.liveDirectory.once('ready', cb);
 	}
 
-	public onChange(cb: (...args: any[]) => void) {
-		this.liveDirectory.on('add', cb);
-		this.liveDirectory.on('update', cb);
-		this.liveDirectory.on('delete', cb);
+	private onChange(event: string, cb: (...args: any[]) => void) {
+		this.liveDirectory.on(event, cb);
+	}
+
+	public onAdd(cb: (path: string, file: LiveFile) => void) {
+		this.onChange('add', cb);
+	}
+
+	public onUpdate(cb: (path: string, file: LiveFile) => void) {
+		this.onChange('update', cb);
+	}
+
+	public onDelete(cb: (path: string) => void) {
+		this.onChange('delete', cb);
 	}
 }
