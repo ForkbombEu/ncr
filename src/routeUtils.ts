a8cf29189c3e8cef93bee646e768d2cb444792cb
matteo-cristino <cristino.matteo97@gmail.com>
2025-10-31 12:14:06 +0100

    fix(cors): allow DPoP header in preflight response
    in the previous commit I add it in the wrong location

--- src/routeUtils.ts
+++ src/routeUtils.ts
@@ -98,7 +98,7 @@ const setCorsHeaders = (res: HttpResponse) => {
 	res
 		.writeHeader('Access-Control-Allow-Origin', '*')
 		.writeHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-		.writeHeader('Access-Control-Allow-Headers', 'content-type, authorization');
+		.writeHeader('Access-Control-Allow-Headers', 'content-type, authorization, dpop');
 };
 
 export const runPrecondition = async (preconditionPath: string, data: JSONObject) => {
@@ -382,7 +382,7 @@ export const generateRoute = async (app: TemplatedApp, endpoint: Endpoints, acti
 				.writeStatus('500')
 				.writeHeader('Access-Control-Allow-Origin', '*')
 				.writeHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-				.writeHeader('Access-Control-Allow-Headers', 'content-type, dpop')
+				.writeHeader('Access-Control-Allow-Headers', 'content-type')
 				.end('Aborted');
 		});
 
